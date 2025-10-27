e2e329e1939ff032023a8bb25e720e4e09054281
dhirajmistry98 <dhirajmistry168@gmail.com>
2025-10-28 00:55:48 +0530

    agent-setup

--- src/modules/home/ui/views/home.view.tsx
+++ src/modules/home/ui/views/home.view.tsx
@@ -1,16 +1,10 @@
 "use client";
-import { trpc } from "@/trpc/client";
 
 export const HomeView = () => {
  
-  const { data, isLoading, error } = trpc.hello.useQuery({ text: "Dhiraj" });
-
-  if (isLoading) return <div className="p-4">Loading...</div>;
-  if (error) return <div className="p-4 text-red-500">Error: {error.message}</div>;
-
   return (
-    <div className="flex flex-col p-4 gap-y-4">
-      <p>{data?.greeting}</p>
+    <div>
+       Home View
     </div>
   );
 };
