7d5bdf31d9b8da27f464afe9af8a177a648a97b5
dhirajmistry98 <dhirajmistry168@gmail.com>
2025-11-08 13:06:18 +0530

    Agent filters

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -1,3 +1,4 @@
+import { loadSearchParams } from "@/modules/agents/params";
 import { AgentsListHeader } from "@/modules/agents/ui/components/agents-list-header";
 import {
   AgentsViewError,
@@ -6,15 +7,23 @@ import {
 } from "@/modules/agents/ui/views/agents-view";
 import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { SearchParams } from "nuqs";
 
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
-const Page = async () => {
+interface Props {
+  searchParams: Promise<SearchParams>
+}
+
+const Page = async ({searchParams}:Props) => {
+  const filters = await loadSearchParams(searchParams);
   
 const queryClient = getQueryClient();
 
-void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
+void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions({
+  ...filters,
+}));
   return (
     <>
     <AgentsListHeader/>
