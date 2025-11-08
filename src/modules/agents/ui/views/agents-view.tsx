75333c055d17f25c8f2524cddf13fa3af877ae88
dhirajmistry98 <dhirajmistry168@gmail.com>
2025-11-09 02:42:10 +0530

    agent page

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -9,37 +9,50 @@ import { columns } from "../components/columns";
 import { EmptyState } from "@/components/empty-state";
 import { useAgentsFilters } from "../../hooks/use-agents-filters";
 import { DataPagination } from "../components/data-pagination";
+import { useRouter } from "next/navigation";
 
 export const AgentsViews = () => {
+  const router = useRouter();
 
-   const [filters ,setFilters] = useAgentsFilters();
+  const [filters, setFilters] = useAgentsFilters();
   const trpc = useTRPC();
-  const  { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions({
-    ...filters,
-  }));
+  const { data } = useSuspenseQuery(
+    trpc.agents.getMany.queryOptions({
+      ...filters,
+    })
+  );
 
   return (
     <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
-     <DataTable data={data.items} columns={columns}/>
-     <DataPagination 
-       page={filters.page}
-       totalPages={data.totalPages}
-        onPageChange={(page) => setFilters({page})}
-     />
-     {data.items.length === 0 &&(
-      <EmptyState title="create your first agent" description="create a agent to join your meetings. Each agents will follow your instructions and can interact with participants during the call"/>
-     )}
+      <DataTable data={data.items} columns={columns}  onRowClick={(row)=> router.push(`/agents/${row.id}`)}/>
+      <DataPagination
+        page={filters.page}
+        totalPages={data.totalPages}
+        onPageChange={(page) => setFilters({ page })}
+      />
+      {data.items.length === 0 && (
+        <EmptyState
+          title="create your first agent"
+          description="create a agent to join your meetings. Each agents will follow your instructions and can interact with participants during the call"
+        />
+      )}
     </div>
   );
 };
 
 export const AgentsViewLoading = () => {
   return (
-    <LoadingState title="Loading Agents" description="This may take a few seconds"/>
-  )
-}
+    <LoadingState
+      title="Loading Agents"
+      description="This may take a few seconds"
+    />
+  );
+};
 export const AgentsViewError = () => {
   return (
-    <ErrorState title="Error Loading Agents" description="Some thing want wrong"/>
-  )
-}
+    <ErrorState
+      title="Error Loading Agents"
+      description="Some thing want wrong"
+    />
+  );
+};
