0f454bca221d8e2df6e363dbb5740d10d18fbf44
dhirajmistry98 <dhirajmistry168@gmail.com>
2025-11-19 13:05:43 +0530

    meetings-filters

--- src/app/(dashboard)/meetings/page.tsx
+++ src/app/(dashboard)/meetings/page.tsx
@@ -11,29 +11,39 @@ import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
+import { loadSearchParams } from "@/modules/meetings/params";
+import type { SearchParams } from "nuqs/server";
 
-const Page = async () => {
-  const session =  await auth.api.getSession({
-   headers: await headers(),
+interface Props {
+  searchParams: Promise<SearchParams>;
+}
+const Page = async ({ searchParams }: Props) => {
+
+  const filters = await loadSearchParams(searchParams);
+
+  const session = await auth.api.getSession({
+    headers: await headers(),
   });
-  
+
   if (!session) {
     redirect("/sign-in");
   }
-    
+
   const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.meetings.getMany.queryOptions({}));
+  void queryClient.prefetchQuery(trpc.meetings.getMany.queryOptions({
+    ...filters,
+  }));
   return (
     <>
-    <MeetingsListHeader />
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<MeetingsViewLoading />}>
-        <ErrorBoundary fallback={<MeetingsViewError />}>
-          <MeetingView />
-        </ErrorBoundary>
-      </Suspense>
-    </HydrationBoundary>
-  </>
+      <MeetingsListHeader />
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<MeetingsViewLoading />}>
+          <ErrorBoundary fallback={<MeetingsViewError />}>
+            <MeetingView />
+          </ErrorBoundary>
+        </Suspense>
+      </HydrationBoundary>
+    </>
   );
 };
 export default Page;
